b9ff5b3d6f9c7543dd4f6f211bf738031271886b
Nastya <treninanst@gmail.com>
2023-07-25 21:06:32 +0400

    Added adta and create an array

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача: Написать программу, которая из имеющегося массива
+//  строк формирует новый массив из строк, длина которых меньше, 
+//  либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//  либо задать на старте выполнения алгоритма. 
+//  При решении не рекомендуется пользоваться коллекциями, 
+//  лучше обойтись исключительно массивами.
+
+Console.WriteLine("Введите значение элемента 1");
+string first = Console.ReadLine()!;
+Console.WriteLine("Введите значение элемента 2");
+string second = Console.ReadLine()!;
+Console.WriteLine("Введите значение элемента 3");
+string third = Console.ReadLine()!;
+
+
+string[] Array = Main(first,second,third); 
+SecondArray(Array);
+
+static string[] Main ( string first, string second, string third)
+{
+    List<string> list = new List <string> { first, second, third};
+    string[] array = list.ToArray();
+    return array;
+}
